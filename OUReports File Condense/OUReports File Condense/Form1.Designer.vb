d0ae3314f4c75c65b53ef91d8e2c14436fb2598e
Shehzad Anwar <cougar59912@gmail.com>
2023-11-29 01:34:19 -0800

    Add files via upload

---
+++ OUReports File Condense/OUReports File Condense/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        FileUploadButton = New Button()
+        SelectedFileLabel = New Label()
+        Title = New Label()
+        FileTypeDropdown = New ComboBox()
+        Description = New Label()
+        FileTypeLabel = New Label()
+        ProgressBar1 = New ProgressBar()
+        ChooseFolder = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' FileUploadButton
+        ' 
+        FileUploadButton.Location = New Point(103, 418)
+        FileUploadButton.Name = "FileUploadButton"
+        FileUploadButton.Size = New Size(298, 23)
+        FileUploadButton.TabIndex = 0
+        FileUploadButton.Text = "Upload File"
+        FileUploadButton.UseVisualStyleBackColor = True
+        ' 
+        ' SelectedFileLabel
+        ' 
+        SelectedFileLabel.ForeColor = SystemColors.Highlight
+        SelectedFileLabel.Location = New Point(103, 444)
+        SelectedFileLabel.Name = "SelectedFileLabel"
+        SelectedFileLabel.Size = New Size(298, 47)
+        SelectedFileLabel.TabIndex = 2
+        ' 
+        ' Title
+        ' 
+        Title.Font = New Font("Segoe UI", 24.0F, FontStyle.Regular, GraphicsUnit.Point)
+        Title.Location = New Point(69, 24)
+        Title.Name = "Title"
+        Title.Size = New Size(343, 148)
+        Title.TabIndex = 3
+        Title.Text = "OUReports Condense File Application"
+        Title.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' FileTypeDropdown
+        ' 
+        FileTypeDropdown.DropDownStyle = ComboBoxStyle.DropDownList
+        FileTypeDropdown.FormattingEnabled = True
+        FileTypeDropdown.Items.AddRange(New Object() {"CSV", "XML", "JSON"})
+        FileTypeDropdown.Location = New Point(103, 283)
+        FileTypeDropdown.Name = "FileTypeDropdown"
+        FileTypeDropdown.Size = New Size(134, 23)
+        FileTypeDropdown.TabIndex = 4
+        ' 
+        ' Description
+        ' 
+        Description.Font = New Font("Segoe UI", 12.0F, FontStyle.Regular, GraphicsUnit.Point)
+        Description.Location = New Point(69, 126)
+        Description.Name = "Description"
+        Description.Size = New Size(343, 128)
+        Description.TabIndex = 5
+        Description.Text = "This Application allows you to upload a either csv, xml, or json files and will condense the file into 4MB chunks and store them into a new folder on your computer."
+        Description.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' FileTypeLabel
+        ' 
+        FileTypeLabel.AutoSize = True
+        FileTypeLabel.Location = New Point(250, 286)
+        FileTypeLabel.Name = "FileTypeLabel"
+        FileTypeLabel.Size = New Size(55, 15)
+        FileTypeLabel.TabIndex = 6
+        FileTypeLabel.Text = "File Type:"
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(103, 494)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(298, 23)
+        ProgressBar1.TabIndex = 7
+        ' 
+        ' ChooseFolder
+        ' 
+        ChooseFolder.Location = New Point(103, 344)
+        ChooseFolder.Name = "ChooseFolder"
+        ChooseFolder.Size = New Size(134, 23)
+        ChooseFolder.TabIndex = 8
+        ChooseFolder.Text = "Choose Folder"
+        ChooseFolder.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.Location = New Point(250, 318)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(204, 36)
+        Label1.TabIndex = 9
+        Label1.Text = "Choose a Folder where the reduced files will be saved"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 8.0F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(250, 354)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(224, 13)
+        Label2.TabIndex = 10
+        Label2.Text = "(Default to where uploaded file is located)"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSizeMode = AutoSizeMode.GrowAndShrink
+        AutoValidate = AutoValidate.EnablePreventFocusChange
+        ClientSize = New Size(502, 549)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(ChooseFolder)
+        Controls.Add(ProgressBar1)
+        Controls.Add(FileTypeLabel)
+        Controls.Add(Description)
+        Controls.Add(FileTypeDropdown)
+        Controls.Add(Title)
+        Controls.Add(SelectedFileLabel)
+        Controls.Add(FileUploadButton)
+        Name = "Form1"
+        Text = "OUReports Condense File"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents FileUploadButton As Button
+    Friend WithEvents SelectedFileLabel As Label
+    Friend WithEvents Title As Label
+    Friend WithEvents FileTypeDropdown As ComboBox
+    Friend WithEvents Description As Label
+    Friend WithEvents FileTypeLabel As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ChooseFolder As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ OUReports File Condense/OUReports File Condense/Form1.vb
@@ -0,0 +1,358 @@
+﻿Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports Microsoft.VisualBasic.FileIO
+Imports System.Xml
+Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+    Dim outputFolder As String = ""
+    Private Sub ChoosesFolderButton_Click(sender As Object, e As EventArgs) Handles ChooseFolder.Click
+        ' Create a new instance of FolderBrowserDialog
+        Dim folderBrowserDialog As New FolderBrowserDialog()
+
+        ' Show the FolderBrowserDialog and check if the user clicked OK
+        If folderBrowserDialog.ShowDialog() = DialogResult.OK Then
+            ' Get the selected folder path
+            Dim selectedFolderPath As String = folderBrowserDialog.SelectedPath
+
+            ' Ask the user for a new folder name
+            Dim newFolderName As String = InputBox("Enter a new folder name:" & vbCrLf & "(Default to Uploaded File Name if left blank.)", "New Folder")
+
+            ' Check if the user entered a folder name
+            If Not String.IsNullOrEmpty(newFolderName) Then
+                ' Combine the selected folder path with the new folder name
+                outputFolder = Path.Combine(selectedFolderPath, newFolderName)
+
+                ' Create the new folder
+                Directory.CreateDirectory(outputFolder)
+            End If
+        End If
+    End Sub
+
+    Private Sub FileUploadButton_Click(sender As Object, e As EventArgs) Handles FileUploadButton.Click
+        ' Resets the progress bar
+        UpdateProgressBar(0)
+        ' Create an instance of the OpenFileDialog
+        Dim openFileDialog As New OpenFileDialog()
+
+        ' Set the filter for the file types you want to allow
+        Dim selectedValue As String = FileTypeDropdown.SelectedItem
+        ' Check if an item is selected
+        If selectedValue IsNot Nothing Then
+            ' Convert the selected value to the appropriate data type
+            Dim selectedText As String = selectedValue
+            If selectedText = "CSV" Then
+                openFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+            ElseIf selectedText = "XML" Then
+                openFileDialog.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*"
+            ElseIf selectedText = "JSON" Then
+                openFileDialog.Filter = "JSON Files (*.json)|*.json|All Files (*.*)|*.*"
+            End If
+
+            ' Show the dialog and get the result
+            Dim result As DialogResult = openFileDialog.ShowDialog()
+
+            ' Check if the user selected a file
+            If result = DialogResult.OK Then
+                ' Display the selected file in the Label
+                SelectedFileLabel.Text = "File Uploaded: " & openFileDialog.FileName
+
+                If selectedText = "CSV" Then
+                    SplitCSV(openFileDialog.FileName)
+                ElseIf selectedText = "XML" Then
+                    SplitXML(openFileDialog.FileName)
+                ElseIf selectedText = "JSON" Then
+                    SplitJSON(openFileDialog.FileName)
+                End If
+
+            End If
+        Else
+            MessageBox.Show("File Type Was Not Selected.")
+        End If
+
+    End Sub
+
+    Private Sub SplitCSV(ByVal inputFileName As String)
+        Try
+            ' Define constants and variables for splitting files
+            Const MaxSize As Long = 4 * 1024 * 1024 ' 4 MB
+            Dim FileNumber As Integer = 1
+            Dim FileSize As Long = 0
+            Dim folderBrowserDialog As New FolderBrowserDialog()
+            Dim InputFileBaseName As String = Path.GetFileNameWithoutExtension(inputFileName)
+
+            ' Creates a folder based on the input file name
+            If outputFolder = "" Then
+                outputFolder = Path.Combine(Path.GetDirectoryName(inputFileName), InputFileBaseName)
+            End If
+            Directory.CreateDirectory(outputFolder)
+
+            Dim OutputName As String = Path.Combine(outputFolder, InputFileBaseName & FileNumber.ToString("D3") & ".csv")
+            Dim OutputFile As New StreamWriter(OutputName)
+
+            ' Initializing the progress bar
+            ProgressBar1.Value = 0
+            ProgressBar1.Maximum = 100
+
+            Using parser As New TextFieldParser(inputFileName)
+                parser.TextFieldType = FieldType.Delimited
+                parser.SetDelimiters(",")
+
+                ' Get the first line to get categories and write it to the first output file
+                Dim categoryLine As String = parser.ReadLine()
+                OutputFile.WriteLine(categoryLine)
+                FileSize += categoryLine.Length + 2 ' Add 2 bytes for newline characters
+
+                ' Calculating progress to update the progress bar
+                Dim totalLines As Long = File.ReadAllLines(inputFileName).Length
+                Dim linesProcessed As Long = 0
+
+                ' Goes through the data and split it into smaller files
+                While Not parser.EndOfData
+                    Dim line As String = parser.ReadLine()
+
+                    ' Check if adding this line would exceed the maximum size of the output file
+                    If FileSize + line.Length + 2 > MaxSize Then
+                        ' Close the current output file and increment the file number
+                        OutputFile.Close()
+                        FileNumber += 1
+
+                        ' Create a new output file with the updated name and write the header line to it
+                        OutputName = Path.Combine(outputFolder, InputFileBaseName & FileNumber.ToString("D3") & ".csv")
+                        OutputFile = New StreamWriter(OutputName)
+                        OutputFile.WriteLine(categoryLine)
+                        FileSize = categoryLine.Length + 2 ' Reset the file size to include only the header line
+                    End If
+
+                    ' Write the current line to the output file and update the file size
+                    OutputFile.WriteLine(line)
+                    FileSize += line.Length + 2
+
+                    linesProcessed += 1
+                    Dim progress As Integer = CInt((linesProcessed / totalLines) * 100)
+                    UpdateProgressBar(progress) ' Update the progress bar
+                End While
+            End Using
+
+            ' Close the last output file
+            OutputFile.Close()
+
+            ' Set the progbar to 100% once the process is done
+            UpdateProgressBar(100)
+
+            MessageBox.Show("The CSV file was successfully split into " & FileNumber & " smaller files in folder " & outputFolder, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("An error occurred while splitting the CSV file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        outputFolder = ""
+    End Sub
+
+    Private Sub SplitXML(ByVal inputFileName As String)
+        Try
+            ' Define the maximum size of each chunk in bytes
+            Const MaxSize As Long = 4 * 1024 * 1024 ' 4 MB
+            Dim FileNumber As Integer = 1
+            Dim FileSize As Long = 0
+            Dim InputFileBaseName As String = Path.GetFileNameWithoutExtension(inputFileName)
+            If outputFolder = "" Then
+                outputFolder = Path.Combine(Path.GetDirectoryName(inputFileName), InputFileBaseName)
+            End If
+            Dim rootElementName As String = Nothing ' To store the root element name
+
+            ' Create the output folder if it doesn't exist
+            Directory.CreateDirectory(outputFolder)
+
+            ' Create an XmlReader object to read the input file
+            Using reader As XmlReader = XmlReader.Create(inputFileName)
+                Dim writerSettings As New XmlWriterSettings()
+                writerSettings.Indent = True
+                writerSettings.Encoding = System.Text.Encoding.UTF8
+
+                ' Create an XmlWriter object to write the output file
+                Dim writer As XmlWriter = Nothing
+                While reader.Read()
+                    If reader.NodeType = XmlNodeType.Element And reader.Depth = 0 Then
+                        ' Capture the root element name if it hasn't been captured already
+                        If rootElementName Is Nothing Then
+                            rootElementName = reader.Name
+                        End If
+                    End If
+
+
+                    If reader.NodeType = XmlNodeType.Element And reader.Depth > 0 Then
+                        If writer Is Nothing Then
+                            ' Create a new writer for the output file
+                            Dim outputFileName As String = Path.Combine(outputFolder, $"{InputFileBaseName}{FileNumber:D3}.xml")
+                            writer = XmlWriter.Create(outputFileName, writerSettings)
+
+                            ' Write the XML declaration for the output file
+                            writer.WriteStartDocument()
+
+                            ' Write the root element using the captured root element name
+                            writer.WriteStartElement(rootElementName)
+                        End If
+
+                        ' Write the current element, including attributes and inner text
+                        writer.WriteNode(reader, True)
+
+                        ' Calculate the current size of the output file
+                        Dim outputFileInfo As New FileInfo(Path.Combine(outputFolder, $"{InputFileBaseName}{FileNumber:D3}.xml"))
+                        FileSize = outputFileInfo.Length
+
+                        ' Check if the current size exceeds the maximum size
+                        If FileSize >= MaxSize Then
+                            ' Close and dispose the current writer
+                            writer.WriteEndElement()
+                            writer.WriteEndDocument()
+                            writer.Close()
+                            writer.Dispose()
+
+                            ' Increment the file number
+                            FileNumber += 1
+
+                            ' Create a new writer for the next output file
+                            Dim newOutputFileName As String = Path.Combine(outputFolder, $"{InputFileBaseName}{FileNumber:D3}.xml")
+                            writer = XmlWriter.Create(newOutputFileName, writerSettings)
+
+                            ' Write the XML declaration for the new output file
+                            writer.WriteStartDocument()
+
+                            ' Write the root element using the captured root element name
+                            writer.WriteStartElement(rootElementName)
+                        End If
+
+                        ' Calculates the progress as a percentage
+                        Dim totalChunks As Integer = FileNumber
+                        Dim currentProgress As Integer = CInt((FileSize / MaxSize) * 100)
+                        UpdateProgressBar(currentProgress) ' Calls to update the progbar
+                    End If
+                End While
+
+                ' Close and dispose the last writer
+                If writer IsNot Nothing Then
+                    writer.WriteEndElement()
+                    writer.WriteEndDocument()
+                    writer.Close()
+                    writer.Dispose()
+                End If
+
+                ' Close and dispose the last writer
+                If writer IsNot Nothing Then
+                    writer.WriteEndElement()
+                    writer.WriteEndDocument()
+                    writer.Close()
+                    writer.Dispose()
+                End If
+
+                ' Set the progbar to 100% once the process is done
+                UpdateProgressBar(100)
+            End Using
+
+            MessageBox.Show($"The XML file '{inputFileName}' was successfully split into {FileNumber} smaller files in folder '{outputFolder}'.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("An error occurred while splitting the XML file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        outputFolder = ""
+    End Sub
+
+    Private Sub SplitJSON(ByVal inputFileName As String)
+        Try
+            ' Define the maximum size of each chunk in bytes
+            Dim MaxSize As Integer = 4 * 1024 * 1024 ' 4MB
+            Dim FileNumber As Integer = 1
+            Dim startIndex As Integer = 0
+            Dim currentArray As JArray = New JArray()
+            Dim InputFileBaseName As String = Path.GetFileNameWithoutExtension(inputFileName)
+            If outputFolder = "" Then
+                outputFolder = Path.Combine(Path.GetDirectoryName(inputFileName), InputFileBaseName)
+            End If
+
+            ' Create the output folder if it doesn't exist
+            Directory.CreateDirectory(outputFolder)
+
+            ' Read the entire input JSON file
+            Dim jsonContent As String = File.ReadAllText(inputFileName)
+            If Not jsonContent.StartsWith("[") Then
+                jsonContent = "[" & jsonContent & "]"
+            End If
+            Dim TotalSize = jsonContent.Length
+            ' Parse the input JSON
+            Dim json As JArray = JArray.Parse(jsonContent)
+
+            ' Calculate the total number of parts based on the chunk size
+            Dim totalParts = CInt(Math.Ceiling(TotalSize / MaxSize))
+            ' Calculate the progress step for each part
+            Dim progressStep = 100 \ totalParts
+
+            ' Split the JSON array into 4MB parts
+            For Each item As JToken In json
+                ' Add the current item to the current part
+                currentArray.Add(item)
+
+                ' Check if the part size exceeds the chunk size
+                If currentArray.ToString().Length > MaxSize Then
+                    ' Save the current part as a separate JSON file
+                    Dim partContent = currentArray.ToString()
+                    Dim outputFilePath = Path.Combine(outputFolder, $"{InputFileBaseName}{FileNumber:D3}.json")
+                    File.WriteAllText(outputFilePath, partContent)
+
+                    ' Update variables for the next part
+                    currentArray = New JArray()
+                    FileNumber += 1
+                    startIndex = 0
+                End If
+
+                ' Calculates the progress as a percentage based on the current part
+                Dim currentProgress As Integer = (FileNumber - 1) * progressStep + CInt((startIndex / TotalSize) * progressStep)
+                UpdateProgressBar(currentProgress) ' Calls to update the progress bar
+
+                ' Update the starting index for the next part
+                startIndex += item.ToString().Length
+            Next
+
+            ' If there are remaining items in the current part, save it
+            If currentArray.Count > 0 Then
+                Dim partContent = currentArray.ToString()
+                Dim outputFilePath = Path.Combine(outputFolder, $"{InputFileBaseName}{FileNumber:D3}.json")
+                File.WriteAllText(outputFilePath, partContent)
+            End If
+
+            ' Set the progbar to 100% once the process is done
+            UpdateProgressBar(100)
+
+            MessageBox.Show($"The JSON file '{inputFileName}' was successfully split into {FileNumber} smaller files in folder '{outputFolder}'.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("An error occurred while splitting the Json file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        outputFolder = ""
+    End Sub
+
+
+    Private Sub UpdateProgressBar(progress As Integer)
+        ' Checks that the progress value is within the valid range (0-100)
+        If progress < 0 Then
+            progress = 0
+        ElseIf progress > 100 Then
+            progress = 100
+        End If
+
+        ' Update the progress bar
+        ProgressBar1.Value = progress
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+
+    End Sub
+
+    Private Sub FileTypeLabel_Click(sender As Object, e As EventArgs) Handles FileTypeLabel.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
